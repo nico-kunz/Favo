05bd0eb360f6d8ab4c7b4913dc5cf60beda54a02
Nico K <nicokunznk00@gmail.com>
2018-02-25 20:53:58 +0100

    Erzwungener Fehler gelöscht

--- Favo/Favo/Form1.cs
+++ Favo/Favo/Form1.cs
@@ -19,7 +19,6 @@ namespace Favo
         {
             InitializeComponent();
             register = new Registers();
-            RegisterMachine Rm = new RegisterMachine(new List<string>() {"load 1","fail 23"});
         }
 
         // Event Handler for the "Speichern als.." item from the ToolStripMenu
