1acca7f93e8e1ac1d11637c04a99d586b5374965
joynere <36499537+joynere@users.noreply.github.com>
2018-03-15 21:25:19 +0100

    Revert "Savedbool/Savemeldung"

--- Favo/Favo/Form1.cs
+++ Favo/Favo/Form1.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -43,132 +43,12 @@ namespace Favo
         public Point mouseLocation;
         Registers register;
         private static string openPath;
-        bool saved = true;
 
 
 
 
 
         // Event Handler for the "Speichern als.." item from the MenuStrip
-        private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            // Get path from SaveFileDialog, save TextEditorBox content at path
-            string s = Dialog.SaveFileDialog();
-            openPath = s;
-
-            if (s != null)
-                FileHandler.SaveFileContent(s, TextEditorBox.Text);
-
-            saved = true;
-
-        }
-
-        // Event Handler for the "Speichern" item from the MenuStrip
-        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            // Execute SaveAs method when openPath not initialized
-            if (openPath != null)
-                FileHandler.SaveFileContent(openPath, TextEditorBox.Text);
-            else
-                SaveAsToolStripMenuItem_Click(null, null);
-
-            saved = true;
-        }
-
-        // Event Handler for the "Öffnen" item from the MenuStrip
-        private void ÖffnenToolStripMenuItem1Click(object sender, EventArgs e)
-        {
-            if (!saved)
-            {
-                DialogResult dialogResult = MessageBox.Show(
-                    "Änderungen am Code speichern?", "Ungespeicherte Änderungen",
-                    MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
-
-                if (dialogResult == DialogResult.Yes)
-                    SaveToolStripMenuItem_Click(null, null);
-            }
-            // Get file path from LoadFileDialog, read file from path and set TextEditorBox.Text to Filetext
-            string s = Dialog.LoadFileDialog();
-            openPath = s;
-
-            if (s != null)
-                TextEditorBox.Text = String.Join(System.Environment.NewLine, FileHandler.GetFileContent(s));
-
-            saved = true;
-
-        }
-
-        // Event Handler for the "Neu" item from the MenuStrip, resets all variables and TextEditorBox.Text
-        private void NeuToolStripMenuItem1Click(object sender, EventArgs e)
-        {
-            openPath = null;
-            if (!saved)
-            {
-                DialogResult dialogResult = MessageBox.Show(
-                    "Änderungen am Code speichern?", "Ungespeicherte Änderungen",
-                    MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
-
-                if (dialogResult == DialogResult.Yes)
-                    SaveToolStripMenuItem_Click(null, null);
-            }
-            TextEditorBox.Text = "";
-
-            saved = true;
-        }
-
-        // Event Handler for the "Schließen" item from the MenuStrip
-        private void CToolStripMenuItemClick_Click(object sender, EventArgs e)
-        {
-            if (!saved)
-            {
-                DialogResult dialogResult = MessageBox.Show(
-                    "Änderungen am Code speichern?", "Ungespeicherte Änderungen",
-                    MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
-
-                if (dialogResult == DialogResult.Yes)
-                    SaveToolStripMenuItem_Click(null, null);
-            }
-
-            Application.Exit();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (!saved)
-            {
-                DialogResult dialogResult = MessageBox.Show(
-                    "Änderungen am Code speichern?", "Ungespeicherte Änderungen",
-                    MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
-
-                if (dialogResult == DialogResult.Yes)
-                    SaveToolStripMenuItem_Click(null, null);
-            }
-
-            Application.Exit();
-        }
-
-        private void panel1_MouseDown(object sender, MouseEventArgs e)
-        {
-            mouseLocation = new Point(-e.X, -e.Y);
-        }
-
-        private void panel1_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                Point mousePosition = Control.MousePosition;
-                mousePosition.Offset(mouseLocation.X, mouseLocation.Y);
-                Location = mousePosition;
-            }
-        }
-
-        private void TextEditorBox_TextChanged(object sender, EventArgs e)
-        {
-            saved = false;
-        }
-    }
-}
-// Event Handler for the "Speichern als.." item from the MenuStrip
         private void SaveasToolStripMenuItemClick(object sender, EventArgs e)
         {
             // Get path from SaveFileDialog, save TextEditorBox content at path
